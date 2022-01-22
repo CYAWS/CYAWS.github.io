badba9b7e08af8a5a0193a1cf5606cf0c180ab28
CYAWS <2437539207@qq.com>
2022-01-22 20:44:35 +0800

    4516fs1a56ds

--- themes/next/scripts/AutoOpenEditor.js
+++ themes/next/scripts/AutoOpenEditor.js
@@ -1,5 +1,5 @@
-let spawn = require('hexo-util/lib/spawn');
+let spawn = require('hexo-util/lib/spawn') ;
 
 hexo.on('new', (data) => {
-  spawn('code', [hexo.base_dir, data.path]);
-});
+  spawn('code', [hexo.base_dir, data.path]) ;
+}) ;
